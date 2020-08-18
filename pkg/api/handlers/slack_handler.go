3008c5c7b54d73f663df742e9e2170c8ed8e30ff
Anna <annaxp@gmail.com>
2020-08-17 22:45:09 -0700

    Add post message on slash command

--- pkg/api/handlers/slack_handler.go
+++ pkg/api/handlers/slack_handler.go
@@ -2,14 +2,12 @@ package handlers
 
 import (
 	"fmt"
-	"bytes"
 	"encoding/json"
 	"net/http"
 
 	"go.uber.org/zap"
 	Conf "github.com/annaworks/chatservice/pkg/conf"
 	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
 )
 
 type Slack_handler struct {
@@ -26,42 +24,71 @@ func NewSlackHandler(logger *zap.Logger, conf Conf.Conf) Slack_handler {
 	}
 }
 
+const slash_command = "/annabot"
+
 func (s Slack_handler) Events(w http.ResponseWriter, r *http.Request) {
 	s.Logger.Info("Received a slack event")
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	body := buf.String()
-	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(fmt.Sprintf("Error: %+v", err)))
-			s.Logger.Error("Error in parsing event", zap.Error(err))
-			return
-		}
+	slash, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Error: %+v", err)))
+		s.Logger.Error("Error in parsing slash command", zap.Error(err))
+		return
+	}
 
-		if eventsAPIEvent.Type == slackevents.URLVerification {
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
+	switch slash.Command {
+		case slash_command:
+			fmt.Printf("%+v\n", slash.Text)
+			fmt.Printf("%+v\n", slash.UserName)
+
+			// Header Section
+			// headerText := slack.NewTextBlockObject("mrkdwn", "You asked a question", false, false)
+			// headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+			// Divider 
+			// divSection := slack.NewDividerBlock()
+
+			// Fields
+			questionText := fmt.Sprintf("*%v asked:*\n%v", slash.UserName, slash.Text)
+			questionField := slack.NewTextBlockObject("mrkdwn", questionText, false, false)
+
+			fieldSlice := make([]*slack.TextBlockObject, 0)
+			fieldSlice = append(fieldSlice, questionField)
+
+			fieldsSection := slack.NewSectionBlock(nil, fieldSlice, nil)
+
+			// Action Buttons
+			viewBtnTxt := slack.NewTextBlockObject("plain_text", "View", false, false)
+			viewBtn := slack.NewButtonBlockElement("", "view_clicked", viewBtnTxt)
+
+			answerBtnTxt := slack.NewTextBlockObject("plain_text", "Answer", false, false)
+			answerBtn := slack.NewButtonBlockElement("", "answer_clicked", answerBtnTxt).WithStyle("primary")
+
+			actionBlock := slack.NewActionBlock("", viewBtn, answerBtn)
+
+			// Build Message with blocks created above
+			msg := slack.NewBlockMessage(
+				// headerSection,
+				// divSection,
+				fieldsSection,
+				actionBlock,
+			)
+			msg.ResponseType = slack.ResponseTypeInChannel
+
+			b, err := json.MarshalIndent(msg, "", "    ")
+
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			w.Header().Set("Content-Type", "text")
-			w.Write([]byte(r.Challenge))
-			s.Logger.Info("Slack challenge completed")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(b)
+
+			s.Logger.Info("Message with buttons sucessfully sent")
+			return 
+		default:
 			return
-		}
-		if eventsAPIEvent.Type == slackevents.CallbackEvent {
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch ev := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				s.Api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
-			}
-			return
-		}
-	
-	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(fmt.Sprintf("Error: Unknown event")))
-	s.Logger.Error("Error receiving unknown slack event")
+	}
 }
