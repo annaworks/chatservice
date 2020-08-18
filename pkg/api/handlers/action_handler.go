8b2fa30fcc441150091dc4003a2ea6249102deb3
Anna <annaxp@gmail.com>
2020-08-18 00:49:56 -0700

    Add modals for view and answer

--- pkg/api/handlers/action_handler.go
+++ pkg/api/handlers/action_handler.go
@@ -24,6 +24,70 @@ func NewActionHandler(logger *zap.Logger, conf Conf.Conf) Action_handler {
 	}
 }
 
+func newViewRequest() slack.ModalViewRequest {
+	// Create a ModalViewRequest with a header and two inputs
+	titleText := slack.NewTextBlockObject("plain_text", "View Answers", false, false)
+	closeText := slack.NewTextBlockObject("plain_text", "Close", false, false)
+	submitText := slack.NewTextBlockObject("plain_text", "Submit", false, false)
+
+	headerContent := fmt.Sprintf("*Question: *")
+	headerText := slack.NewTextBlockObject("mrkdwn", headerContent, false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	firstNameText := slack.NewTextBlockObject("plain_text", "First Name", false, false)
+	firstNamePlaceholder := slack.NewTextBlockObject("plain_text", "Enter your first name", false, false)
+	firstNameElement := slack.NewPlainTextInputBlockElement(firstNamePlaceholder, "firstName")
+	// Notice that blockID is a unique identifier for a block
+	firstName := slack.NewInputBlock("First Name", firstNameText, firstNameElement)
+
+	lastNameText := slack.NewTextBlockObject("plain_text", "Last Name", false, false)
+	lastNamePlaceholder := slack.NewTextBlockObject("plain_text", "Enter your first name", false, false)
+	lastNameElement := slack.NewPlainTextInputBlockElement(lastNamePlaceholder, "lastName")
+	lastName := slack.NewInputBlock("Last Name", lastNameText, lastNameElement)
+
+	blocks := slack.Blocks{
+		BlockSet: []slack.Block{
+			headerSection,
+			firstName,
+			lastName,
+		},
+	}
+
+	var modalRequest slack.ModalViewRequest
+	modalRequest.Type = slack.ViewType("modal")
+	modalRequest.Title = titleText
+	modalRequest.Close = closeText
+	modalRequest.Submit = submitText
+	modalRequest.Blocks = blocks
+	return modalRequest
+}
+
+func newAnswerRequest() slack.ModalViewRequest {
+	// Create a ModalViewRequest with a header and two inputs
+	titleText := slack.NewTextBlockObject("plain_text", "Add an Answer", false, false)
+	closeText := slack.NewTextBlockObject("plain_text", "Cancel", false, false)
+	submitText := slack.NewTextBlockObject("plain_text", "Submit", false, false)
+
+	answerText := slack.NewTextBlockObject("plain_text", "How to setup a go api?", false, false)
+	answerPlaceholder := slack.NewTextBlockObject("plain_text", "Write something", false, false)
+	answerElement := slack.NewPlainTextInputBlockElement(answerPlaceholder, "answer")
+	answer := slack.NewInputBlock("Answer 1", answerText, answerElement)
+
+	blocks := slack.Blocks{
+		BlockSet: []slack.Block{
+			answer,
+		},
+	}
+
+	var modalRequest slack.ModalViewRequest
+	modalRequest.Type = slack.ViewType("modal")
+	modalRequest.Title = titleText
+	modalRequest.Close = closeText
+	modalRequest.Submit = submitText
+	modalRequest.Blocks = blocks
+	return modalRequest
+}
+
 func (s Action_handler) Events(w http.ResponseWriter, r *http.Request) {
 	s.Logger.Info("Received a slack action")
 
@@ -35,6 +99,24 @@ func (s Action_handler) Events(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(fmt.Sprintf("Error: Unknown event")))
 		s.Logger.Error("Error receiving unknown slack event")
+		return
+	}
+
+	switch payload.ActionCallback.BlockActions[0].Value {
+	case "view_clicked":
+		modalRequest := newViewRequest()
+		_, err = s.Api.OpenView(payload.TriggerID, modalRequest)
+		if err != nil {
+			fmt.Printf("Error opening view: %s", err)
+		}
+	case "action_clicked":
+		modalRequest := newAnswerRequest()
+		_, err = s.Api.OpenView(payload.TriggerID, modalRequest)
+		if err != nil {
+			fmt.Printf("Error opening view: %s", err)
+		}
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
